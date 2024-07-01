b8eceb60f1eb80096b60ee4bea6643b360600ed5
Rohan Konda <78698358+RoKo87@users.noreply.github.com>
2024-07-01 14:17:18 -0500

    Add files via upload

--- AquifyATXloginpage/loginpage/core/authentication/ViewModel/MapViewModel.swift
+++ AquifyATXloginpage/loginpage/core/authentication/ViewModel/MapViewModel.swift
@@ -9,47 +9,50 @@ import Foundation
 import MapKit
 import CoreLocation
 
-enum MapDet {
-    static let startLoc = CLLocationCoordinate2D(latitude: 30, longitude: -96.5)
-    static let defSpan = MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
-}
-
-final class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
-    
-    @Published var region = MKCoordinateRegion(center: MapDet.startLoc, span: MapDet.defSpan)
-    
-    var locationManager: CLLocationManager?
-    
-    //check if location services are enabled
-    func checkLocServices() {
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager = CLLocationManager()
-            locationManager!.delegate = self
-        } else {
-            print("show alert")
-        }
-    }
-    
-    // check location authorization
-    private func checkLocAuth() {
-        guard let locationManager = locationManager else {return}
-        
-        switch locationManager.authorizationStatus {
-            
-        case .notDetermined:
-            locationManager.requestWhenInUseAuthorization()
-        case .restricted:
-            print("alert: restricted")
-        case .denied:
-            print("alert: denied")
-        case .authorizedAlways, .authorizedWhenInUse:
-            region = MKCoordinateRegion(center: locationManager.location!.coordinate, span: MapDet.defSpan)
-        @unknown default:
-            break;
-        }
-    }
-    
-    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        checkLocAuth()
-    }
-}
+//enum MapDet {
+//    static let startLoc = CLLocationCoordinate2D(latitude: 30, longitude: -96.5)
+//    static let defSpan = MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+//}
+//
+//final class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+//    
+//    override init() {
+//        locationManager = CLLocationManager()
+//    }
+//    
+//    var locationManager: CLLocationManager
+//    
+//    //check if location services are enabled
+//    func checkLocServices() {
+//        if CLLocationManager.locationServicesEnabled() {
+//            locationManager = CLLocationManager()
+//            locationManager.delegate = self
+//        } else {
+//            print("show alert")
+//        }
+//    }
+//    
+//    // check location authorization
+//    func checkLocAuth() {
+//        locationManager.requestWhenInUseAuthorization()
+//        
+//        
+//        switch locationManager.authorizationStatus {
+//            
+//        case .notDetermined:
+//            locationManager.requestWhenInUseAuthorization()
+//        case .restricted:
+//            print("alert: restricted")
+//        case .denied:
+//            print("alert: denied")
+//        case .authorizedAlways, .authorizedWhenInUse:
+//            let locationManager = CLLocationManager()
+//        @unknown default:
+//            break;
+//        }
+//    }
+//    
+//    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+//        checkLocAuth()
+//    }
+//}
