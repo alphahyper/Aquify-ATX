09dd25cf446849eb67adf23abe0839cab79b3ce5
Rohan Konda <78698358+RoKo87@users.noreply.github.com>
2024-07-04 12:19:36 -0500

    Add files via upload

--- AquifyATXloginpage/loginpage/core/profile/NoUserWarningView.swift
+++ AquifyATXloginpage/loginpage/core/profile/NoUserWarningView.swift
@@ -9,7 +9,36 @@ import SwiftUI
 
 struct NoUserWarningView: View {
     var body: some View {
-        Text("Sorry, you are a guest, you don't have a profile page.")
+        Text("Sorry, you are a guest, you don't have a profile page and you can't access the settings page.")
+        Text("If you would like, you can")
+        
+        //signupbutton
+        NavigationLink{
+            RegistrationView()
+                .navigationBarBackButtonHidden(true)
+        } label:{
+            HStack(spacing: 2){
+                Text("Sign up")
+                    .fontWeight(.bold)
+            }
+            .font(.system(size: 20))
+        }
+        
+        Text("or")
+        
+        //signupbutton
+        NavigationLink{
+            LoginView()
+                .navigationBarBackButtonHidden(true)
+        } label:{
+            HStack(spacing: 2){
+                Text("Log in")
+                    .fontWeight(.bold)
+                Text(".")
+            }
+            .font(.system(size: 20))
+        }
+        
     }
 }
 
