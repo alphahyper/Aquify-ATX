871faaf3152c5afb0741ef60b0878e0ee75ba622
Rohan Konda <78698358+RoKo87@users.noreply.github.com>
2024-07-05 10:34:47 -0500

    Add files via upload

--- AquifyATXloginpage/loginpage/core/authentication/ViewModel/MapViewModel.swift
+++ AquifyATXloginpage/loginpage/core/authentication/ViewModel/MapViewModel.swift
@@ -9,50 +9,53 @@ import Foundation
 import MapKit
 import CoreLocation
 
-//enum MapDet {
-//    static let startLoc = CLLocationCoordinate2D(latitude: 30, longitude: -96.5)
-//    static let defSpan = MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
-//}
-//
-//final class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
-//    
-//    override init() {
-//        locationManager = CLLocationManager()
-//    }
-//    
-//    var locationManager: CLLocationManager
-//    
-//    //check if location services are enabled
-//    func checkLocServices() {
-//        if CLLocationManager.locationServicesEnabled() {
-//            locationManager = CLLocationManager()
-//            locationManager.delegate = self
-//        } else {
-//            print("show alert")
-//        }
-//    }
-//    
-//    // check location authorization
-//    func checkLocAuth() {
-//        locationManager.requestWhenInUseAuthorization()
-//        
-//        
-//        switch locationManager.authorizationStatus {
-//            
-//        case .notDetermined:
-//            locationManager.requestWhenInUseAuthorization()
-//        case .restricted:
-//            print("alert: restricted")
-//        case .denied:
-//            print("alert: denied")
-//        case .authorizedAlways, .authorizedWhenInUse:
-//            let locationManager = CLLocationManager()
-//        @unknown default:
-//            break;
-//        }
-//    }
-//    
-//    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-//        checkLocAuth()
-//    }
-//}
+final class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    @Published private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    @Published var showLSPAlert: Bool = false;
+    @Published var locServicesProper = true {
+        didSet {
+            if !locServicesProper {
+                showLSPAlert = true;
+            }
+        }
+    }
+    
+    var locationManager: CLLocationManager?
+    //?
+    func checkLocServices() {
+        let myQueue = DispatchQueue(label: "custQ")
+        myQueue.async { [self] in
+            if CLLocationManager.locationServicesEnabled() {
+                locationManager = CLLocationManager()
+                locationManager?.delegate = self;
+                //!
+            } else {
+                locServicesProper = false;
+                checkLocAuth()
+            }
+        }
+    }
+    
+    private func checkLocAuth() {
+        
+        guard let locationManager = locationManager else {return}
+        switch locationManager.authorizationStatus {
+            
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            locationManager.requestWhenInUseAuthorization()
+        case .denied:
+            locationManager.requestWhenInUseAuthorization()
+        case .authorizedAlways, .authorizedWhenInUse:
+            region = MKCoordinateRegion(center: locationManager.location!.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+        @unknown default:
+            break
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocAuth()
+    }
+}
--- AquifyATXloginpage/loginpage/core/profile/MapView.swift
+++ AquifyATXloginpage/loginpage/core/profile/MapView.swift
@@ -20,73 +20,83 @@ struct MapView: View {
     var wdlength = WaterData().length
     
     var body: some View {
-        VStack {
-            Map(position: $camera) {
-                ForEach(0...(wd.length - 1), id: \.self) { ind in
-                    Marker(wd.getName(index: ind), systemImage: "drop.fill", coordinate: wd.toCoords(index: ind))
-                        .tint(.blue)
+        NavigationView {
+            VStack {
+                Map(position: $camera) {
+                    ForEach(0...(wd.length - 1), id: \.self) { ind in
+                        Marker(wd.getName(index: ind), systemImage: "drop.fill", coordinate: wd.toCoords(index: ind))
+                            .tint(.blue)
+                    }
+                    
+                    
+                    UserAnnotation()
                 }
-                
-      
-                UserAnnotation()
-            }
                 .onAppear {
                     vm.checkLocServices()
                 }
                 .alert(isPresented: $vm.showLSPAlert) {
                     Alert(title: Text("Alert"), message: Text("this sucks"))
                 }
-            HStack(spacing: 30) {
-                NavigationLink{
-                    if (avm.userSession == nil) {
-                        NoUserWarningView()
-                    }
-                    else { MapView()
-                            .navigationBarBackButtonHidden(true)
-                    }
-                } label:{
-                    VStack(spacing: 4){
-                        Image(systemName: "person.crop.circle")
-                            .padding()
-                            .scaledToFit()
-                            .font(.system(size: 30))
-                        Text("Profile")
-                            .fontWeight(.bold)
-                            .font(.system(size: 14))
-                    }
-                }
                 
-                NavigationLink{
-                    MapView()
-                            .navigationBarBackButtonHidden(true)
-                } label:{
-                    VStack(spacing: 4){
-                        Image(systemName: "map.fill")
-                            .padding()
-                            .scaledToFit()
-                            .font(.system(size: 30))
-                        Text("Map")
-                            .fontWeight(.bold)
-                            .font(.system(size: 14))
-                    }
-                }
                 
-                NavigationLink{
-                    if (avm.userSession == nil) {
-                        NoUserWarningView()
+                HStack(spacing: 30) {
+                    
+                    /* profile link */
+                    NavigationLink{
+                        if (avm.userSession != nil) {
+                            ProfileView()
+                        }
+                        else { NoUserWarningView()
+                                .navigationBarBackButtonHidden(true)
+                        }
+                    } label:{
+                        VStack(spacing: 4){
+                            Image(systemName: "person.crop.circle")
+                                .padding()
+                                .scaledToFit()
+                                .font(.system(size: 30))
+                            Text("Profile")
+                                .fontWeight(.bold)
+                                .font(.system(size: 14))
+                        }
                     }
-                    else { MapView()
+                    
+                    
+                    /* map link */
+                    NavigationLink{
+                        MapView()
                             .navigationBarBackButtonHidden(true)
+                    } label:{
+                        VStack(spacing: 4){
+                            Image(systemName: "map.fill")
+                                .padding()
+                                .scaledToFit()
+                                .font(.system(size: 30))
+                            Text("Map")
+                                .fontWeight(.bold)
+                                .font(.system(size: 14))
+                        }
                     }
-                } label:{
-                    VStack(spacing: 4){
-                        Image(systemName: "gear")
-                            .padding()
-                            .scaledToFit()
-                            .font(.system(size: 30))
-                        Text("Settings")
-                            .fontWeight(.bold)
-                            .font(.system(size: 14))
+                    
+                    
+                    /* settings link */
+                    NavigationLink{
+                        if (avm.userSession != nil) {
+                            ProfileView()
+                        }
+                        else { NoUserWarningView()
+                                .navigationBarBackButtonHidden(true)
+                        }
+                    } label:{
+                        VStack(spacing: 4){
+                            Image(systemName: "gear")
+                                .padding()
+                                .scaledToFit()
+                                .font(.system(size: 30))
+                            Text("Settings")
+                                .fontWeight(.bold)
+                                .font(.system(size: 14))
+                        }
                     }
                 }
             }
@@ -101,53 +111,3 @@ struct MapViewPreviews: PreviewProvider {
     }
 }
 
-final class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
-    
-    @Published private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
-    @Published var showLSPAlert: Bool = false;
-    @Published var locServicesProper = true {
-        didSet {
-            if !locServicesProper {
-                showLSPAlert = true;
-            }
-        }
-    }
-    
-    var locationManager: CLLocationManager?
-    //?
-    func checkLocServices() {
-        let myQueue = DispatchQueue(label: "custQ")
-        myQueue.async { [self] in
-            if CLLocationManager.locationServicesEnabled() {
-                locationManager = CLLocationManager()
-                locationManager?.delegate = self;
-                //!
-            } else {
-                locServicesProper = false;
-                checkLocAuth()
-            }
-        }
-    }
-    
-    private func checkLocAuth() {
-        
-        guard let locationManager = locationManager else {return}
-        switch locationManager.authorizationStatus {
-            
-        case .notDetermined:
-            locationManager.requestWhenInUseAuthorization()
-        case .restricted:
-            locationManager.requestWhenInUseAuthorization()
-        case .denied:
-            locationManager.requestWhenInUseAuthorization()
-        case .authorizedAlways, .authorizedWhenInUse:
-            region = MKCoordinateRegion(center: locationManager.location!.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
-        @unknown default:
-            break
-        }
-    }
-    
-    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        checkLocAuth()
-    }
-}
